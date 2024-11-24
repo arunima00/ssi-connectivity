d547b461338a41eb04b99d3cf8cbd4b9ab778fba
Arunima Jain <arunima.jain00@gmail.com>
2024-11-25 01:38:40 +0530

    Set background pixel values

--- 4. land cover.R
+++ 4. land cover.R
@@ -74,11 +74,11 @@ lc_1995_res <- resample(x = lc_1995,
 # Read shapefile for Nilgiris >1400m
 nil1400 <- vect(paste0(proj_path,"GIS/Shapefiles/Nilgiri1400m/Nilgiri1400m.shp"))
 
-# Crop and mask all rasters to shapefile for faster computation
-lc_2017 <- crop(lc_2017,nil1400,mask = TRUE)
-lc_1995 <- crop(lc_1995,nil1400,mask = TRUE)
+# Crop all rasters to extent of shapefile for faster computation
+lc_2017 <- crop(lc_2017,nil1400)
+lc_1995 <- crop(lc_1995,nil1400)
 
-clim_zone_res <- crop(clim_zone_res,nil1400,mask = TRUE)
+clim_zone_res <- crop(clim_zone_res,nil1400)
 
 # Polygonise rasters
 lc_2017_vect <- as.polygons(lc_2017,round = FALSE)
@@ -112,11 +112,17 @@ names(ptcover_gland_2017) <- "gland_cover"
 ptcover_gland_1995 <- ptcover_1995_rast$`Shola Grassland`
 names(ptcover_gland_1995) <- "gland_cover"
 
-# Write raster to TIF file
-writeRaster(ptcover_gland_2017,
-            filename = paste0(proj_path,"SDM/Input/nil1400_1ha/predictors_all/gland_cover_2017.tif"),
-            overwrite = TRUE)
+# Set background pixels to 0
+ptcover_gland_2017[is.na(ptcover_gland_2017)] <- 0
+ptcover_gland_1995[is.na(ptcover_gland_1995)] <- 0
 
-writeRaster(ptcover_gland_1995,
-            filename = paste0(proj_path,"SDM/Input/nil1400_1ha/predictors_all/gland_cover_1995.tif"),
-            overwrite = TRUE)
+# Mask to Nilgiris 1400m shapefile and write to TIF file
+ptcover_gland_2017_nil <- mask(ptcover_gland_2017,
+                               mask = nil1400,
+                               filename = paste0(proj_path,"SDM/Input/nil1400_1ha/predictors_all/gland_cover_2017.tif"),
+                               overwrite = TRUE)
+
+ptcover_gland_1995_nil <- mask(ptcover_gland_1995,
+                               mask = nil1400,
+                               filename = paste0(proj_path,"SDM/Input/nil1400_1ha/predictors_all/gland_cover_1995.tif"),
+                               overwrite = TRUE)
