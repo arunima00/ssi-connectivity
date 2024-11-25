1b249a36c2579bbf0a8bcf0915d6d6e0e8c93779
Arunima Jain <arunima.jain00@gmail.com>
2024-11-25 23:23:06 +0530

    Change CRS

--- 1. climate zones.R
+++ 1. climate zones.R
@@ -10,14 +10,47 @@ proj_path <- "C:/Users/aruni/arunima/IISERTpt/Connectivity/"
 # Read climate zone raster
 clim_zone <- rast(paste0(proj_path,"GIS/Climate zones/ClassifiedSumSWGPrecandTas.tif"))
 
-# Convert into categorical raster and set levels
-levels(clim_zone) <- data.frame(id = 1:37,zone = 11:47)
+# Specify from-to values for reclassification
+m <- c(17, 1,
+       27, 2,
+       37, 3,
+       47, 4,
+       16, 5, 
+       26, 6,
+       36, 7,
+       46, 8,
+       15, 9,
+       25, 10,
+       35, 11,
+       45, 12,
+       14, 13,
+       24, 14,
+       34, 15,
+       44, 16,
+       13, 17,
+       23, 18,
+       33, 19,
+       43, 20,
+       12, 21,
+       22, 22,
+       32, 23,
+       42, 24,
+       11, 25,
+       21, 26,
+       31, 27,
+       41, 28)
+
+# Convert to matrix
+rclmat <- matrix(m, ncol = 2, byrow = TRUE)
+
+# Reclassify raster
+clim_zone <- classify(clim_zone, rcl = rclmat)
+
+# Convert to categorical raster and setlevels
+levels(clim_zone) <- data.frame(id = 1:28,zone = 1:28)
 
 # Read reference 1ha resolution raster
 rast_1ha <- rast(paste0(proj_path,"GIS/1ha grids.tif"))
-
-# Reproject climate zone raster to reference CRS
-clim_zone <- project(clim_zone,y = crs(rast_1ha))
 
 # Create empty ~1ha resolution raster for resampling
 res <- rast(nrows = nrow(rast_1ha),
@@ -25,15 +58,15 @@ res <- rast(nrows = nrow(rast_1ha),
             crs = crs(rast_1ha),
             ext = ext(rast_1ha))
 
-# Resample to 1ha resolution raster
-clim_zone_res <- resample(x = clim_zone,
-                          y = res,
-                          method = "near")
+# Reproject climate zone raster to reference CRS
+clim_zone <- project(clim_zone,y = crs(rast_1ha))
 
 # Rename layer
-names(clim_zone_res) <- "clim_zone"
+names(clim_zone) <- "clim_zone"
 
-# Write resampled raster to TIF file
-writeRaster(clim_zone_res,
-            filename = paste0(proj_path,"GIS/Climate zones/clim_zone_1ha.tif"),
-            overwrite = TRUE)
+# Resample to 1ha resolution raster and write to TIF file
+clim_zone_res <- resample(x = clim_zone,
+                          y = res,
+                          method = "near",
+                          filename = paste0(proj_path,"GIS/Climate zones/clim_zone_1ha.tif"),
+                          overwrite = TRUE)
