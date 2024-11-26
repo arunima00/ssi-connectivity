41619495aba9c6a4bd5d4b6d2d762a9c8627f72f
Arunima Jain <arunima.jain00@gmail.com>
2024-11-26 21:06:37 +0530

    Change grassland variables

--- 7. correlation.R
+++ 7. correlation.R
@@ -10,21 +10,22 @@ proj_path <- "C:/Users/aruni/arunima/IISERTpt/Connectivity/"
 
 ## Repeat the following process for all raster stacks for present
 
-region <- "nil1400"
-vars <- "gland"
-sc <- "present"
+# Set region of interest
+region <- "swg1400"
+#region <- "nil1400"
+#region <- "pahw1400"
 
 # Read raster stack
-st <- rast(paste0(proj_path,"SDM/Input/",region,"_1ha/",region,"_predictors_",sc,"_",vars,".tif"))
+st <- rast(paste0(proj_path,"SDM/Input/",region,"_1ha/",region,"_predictors_present.tif"))
 
-# Calculate correlation between all pairs of variables
+# Calculate correlation between all pairs of continuous variables
 stack_corr <- layerCor(st,fun = "pearson",na.rm = TRUE)
 
 # Extract correlation matrix
 corr_matrix <- stack_corr$correlation
 
 # Create correlation plot and save as PNG
-png(paste0(proj_path,"SDM/Input/",region,"_1ha/Correlation/",region,"_",vars,"_corrplot.png"))
+png(paste0(proj_path,"SDM/Input/",region,"_1ha/Correlation/",region,"_corrplot.png"))
 corrplot(corr_matrix,
          is.corr = FALSE, 
          method = "square",
@@ -49,7 +50,7 @@ df_th <- droplevels(df[abs(df$value) >= 0.7,])
 
 # Write filtered dataframe to CSV file
 write.csv(df,
-          file = paste0(proj_path,"SDM/Input/",region,"_1ha/Correlation/",region,"_",vars,"_correlation.csv"))
+          file = paste0(proj_path,"SDM/Input/",region,"_1ha/Correlation/",region,"_correlation.csv"))
 
 # Extract variable names
 all_vars <- names(st)
@@ -58,8 +59,8 @@ all_vars # view all variables
 df_th # view correlated variables
 
 # Remove variables with correlation > 0.7 
-fil_vars <- all_vars[! all_vars %in% c("roughness","slope","elevation")]
+fil_vars <- all_vars[! all_vars %in% c("roughness","slope")]
 
 # Save list of filtered variable names
 save(fil_vars,
-     file = paste0(proj_path,"SDM/Input/",region,"_1ha/Correlation/",region,"_",vars,"_fil.RData"))
+     file = paste0(proj_path,"SDM/Input/",region,"_1ha/Correlation/",region,"_fil.RData"))
