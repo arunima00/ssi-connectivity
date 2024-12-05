89ec1f2d1dc4aef7d4776819f6340802cf192b9e
Arunima Jain <arunima.jain00@gmail.com>
2024-12-05 20:12:45 +0530

    Plotting

--- 12. output plotting.R
+++ 12. output plotting.R
@@ -1,5 +1,4 @@
 # Load packages
-library(tidyverse)
 library(terra)
 
 # Clear environment 
@@ -8,46 +7,29 @@ rm(list = ls())
 # Set project path prefix
 proj_path <- "C:/Users/aruni/arunima/IISERTpt/Connectivity/"
 
-# Loop for each scenario - past and present
-for (y in c("1995","2017")) {
+# Get file names for all output files
+files <- list.files(path = paste0(proj_path,"Omniscape/Output"),
+                    pattern = ".tif$",
+                    full.names = TRUE,
+                    recursive = TRUE)
+
+# Loop to plot each output map
+for (i in files) {
   
-  # Get file names for all output files
-  files <- list.files(path = paste0(proj_path,"Omniscape/Output/",y),
-                      pattern = ".tif$",
-                      full.names = TRUE,
-                      recursive = TRUE)
+  # Read raster
+  r <- rast(i)
   
-  # Loop to plot each map
-  for (i in files){
-    
-    # Read raster
-    r <- rast(i)
-    
-    # Plot cumulative current flow and flow potential maps 
-    if (! grepl("normalized_cum_currmap",i,fixed = TRUE)) {
-      
-      # Get quantiles of raster values
-      quantiles <- quantile(values(r), 
-                            probs = seq(0, 1, length.out = 6), 
-                            na.rm = TRUE)
-      
-      # Plot the raster with quantile breaks and save as PNG
-      png(filename=sub(".tif",".png",i))
-      plot(r, 
-           breaks = quantiles,
-           col = map.pal("viridis",5), 
-           mar = c(3.1,3.1,2.1,7.7))
-      dev.off()
-    }
-    
-    # Plot normalised cumulative current flow maps and save as PNG
-    if (grepl("normalized_cum_currmap",i,fixed = TRUE)) {
-      png(filename=sub(".tif",".png",i))
-      plot(r, 
-           breaks = c(0,0.7,1.3,1.7,max(values(r),na.rm = TRUE)),
-           col = map.pal("viridis",4), 
-           mar = c(3.1,3.1,2.1,7.7))
-      dev.off()
-    }
-  }
+  # Convert raster to dataframe
+  r_df <- as.data.frame(r,xy = TRUE,na.rm = FALSE) 
+  
+  # Bin raster values into 100 bins
+  binned <- ntile(r_df[[3]],100)
+  
+  # Rasterize dataframe with binned values
+  r_binned <- rast(cbind(r_df[,c("x","y")],binned),crs = "epsg:4326")
+  
+  # Plot binned raster and save as PNG
+  png(filename = sub(".tif",".png",i))
+  plot(r_binned,col = map.pal("inferno",100))
+  dev.off()
 }
