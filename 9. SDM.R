25fb266a34e2290baceb8afbaa9c44edd66df78b
Arunima Jain <arunima.jain00@gmail.com>
2025-02-14 12:26:57 +0530

    Annotation

--- 9. SDM.R
+++ 9. SDM.R
@@ -11,24 +11,68 @@ rm(list = ls())
 # Set project path prefix
 proj_path <- "C:/Users/aruni/arunima/IISERTpt/Connectivity/"
 
+# Create list of all predictor variables across species
+load(paste0("SDM/Input/nil1400_1ha/Correlation/nil1400_1ha_fil.RData"))
+used_vars <- fil_vars
+
+load(paste0("SDM/Input/pa1400_1ha/Correlation/pa1400_1ha_fil.RData"))
+used_vars <- c(used_vars,fil_vars)
+
+load(paste0("SDM/Input/nilpa1400_1ha/Correlation/nilpa1400_1ha_fil.RData"))
+used_vars <- c(used_vars,fil_vars)
+
+load(paste0("SDM/Input/nilpa1400_25ha/Correlation/nilpa1400_25ha_fil.RData"))
+used_vars <- c(used_vars,fil_vars)
+
+used_vars <- unique(unlist(used_vars))
+
+# Set row and column names for model paramaters matrix
+col <- c("Occurrence type",
+         "Region",
+         "Resolution",
+         "Presences (train)",
+         "Presences (test)",
+         "Absences (train)",
+         "Absences (test)",
+         "AUC (ROC)",
+         "AUC (PRC)",
+         "mtry",
+         "nodesize",
+         used_vars)
+
+spec_list <- c("SHAL","SHMA","MOFA","MOCA","ANNI","EUAL","FINI")
+
+# Create empty matrix
+model_param <- matrix(nrow = length(col),
+                      ncol = length(spec_list),
+                      dimnames = list(col,spec_list))
+
 # Create loop to run SDMs for all species
-for (spec in c("SHAL","SHMA","MOFA","MOCA","ANNI","EUAL","FINI")) {
+for (spec in spec_list) {
   
   # Load species-specific predictor variables
   if (spec %in% c("SHAL","MOFA")){
     region <- "pa1400_1ha"
+    model_param["Region",spec] <- "pa1400"
+    model_param["Resolution",spec] <- "1ha"
   }
   
   if (spec %in% c("SHMA","MOCA")){
     region <- "nil1400_1ha"
+    model_param["Region",spec] <- "nil1400"
+    model_param["Resolution",spec] <- "1ha"
   }
   
   if (spec == "ANNI"){
     region <- "nilpa1400_25ha"
+    model_param["Region",spec] <- "nilpa1400"
+    model_param["Resolution",spec] <- "25ha"
   }
   
   if (spec %in% c("EUAL","FINI")) {
     region <- "nilpa1400_1ha"
+    model_param["Region",spec] <- "nilpa1400"
+    model_param["Resolution",spec] <- "1ha"
   }
   
   predictors <- rast(paste0(proj_path,"SDM/Input/",region,"/predictors_present.tif"))
@@ -49,6 +93,9 @@ for (spec in c("SHAL","SHMA","MOFA","MOCA","ANNI","EUAL","FINI")) {
   # Get data table
   df <- terra::extract(predictors,occ_vect,ID = FALSE,bind = TRUE)
   df <- as.data.frame(df,geom = "XY") %>% na.omit()
+  
+  # Set occurrence data type (Presence-Absence)
+  model_param["Occurrence type",spec] <- "PA"
   
   # Generate pseudo-absences for Nilgiri Pipit using biomod2
   if (spec == "ANNI") {
@@ -76,6 +123,9 @@ for (spec in c("SHAL","SHMA","MOFA","MOCA","ANNI","EUAL","FINI")) {
     df <- data.frame(Presence = BiomodData@data.species,
                      BiomodData@data.env.var,
                      BiomodData@coord)
+    
+    # Set occurrence data type (Presence-Background)
+    model_param["Occurrence type",spec] <- "PB"
   }
   
   # Stratified sampling on presence-absence data
@@ -94,36 +144,49 @@ for (spec in c("SHAL","SHMA","MOFA","MOCA","ANNI","EUAL","FINI")) {
     dir.create(paste0(proj_path,"SDM/Output/Datasets"),recursive = TRUE)
   }
   
+  # Save values for number of presences and absences
+  model_param["Presences (train)",spec] <- nrow(filter(train,Presence == 1))
+  model_param["Absences (train)",spec] <- nrow(filter(train,Presence == 0))
+  model_param["Presences (test)",spec] <- nrow(filter(test,Presence == 1))
+  model_param["Absences (test)",spec] <- nrow(filter(test,Presence == 0))
+  
   # Save training and testing datasets
-  write.csv(train, 
+  write.csv(df[train_index,], 
             file = paste0(proj_path,"SDM/Output/Datasets/",spec,"_",region,"_train.csv"),
             row.names = FALSE)
-  write.csv(test, 
+  write.csv(df[-train_index,], 
             file = paste0(proj_path,"SDM/Output/Datasets/",spec,"_",region,"_test.csv"),
             row.names = FALSE)
   
-  # Creating mlr task for tuning
+  # Create mlr task for tuning
   mlr.task <- mlr::makeClassifTask(data = train, target = "Presence")
   
-  # Tuning specific hyperparameters
+  # Tune specific hyperparameters
   set.seed(25)
   res <- tuneRanger(mlr.task,tune.parameters = c("mtry","min.node.size"))
   
-  # Mean of best 5 % of the results
+  # View mean of best 5 % of the results
   res
   
-  # Run random forest using true absences for forest species
+  # Set hyperparameter values
+  mtry <- res$recommended.pars[["mtry"]]
+  nodesize <- res$recommended.pars[["min.node.size"]]
+  
+  model_param["mtry",spec] <- mtry
+  model_param["nodesize",spec] <- nodesize
+  
+  # Train random forest using true absences for forest species
   if (spec != "ANNI") {
     set.seed(25)
     rf <- randomForest(formula = Presence ~.,
                        data = train,
                        ntree = 1000,
-                       mtry = res$recommended.pars[["mtry"]],
-                       nodesize = res$recommended.pars[["min.node.size"]],
+                       mtry = mtry,
+                       nodesize = nodesize,
                        importance = T)
   }
   
-  # Run downsampled random forest using pseudo-absences for Nilgiri Pipit
+  # Train downsampled random forest using pseudo-absences for grassland species
   if (spec == "ANNI") {
     
     prNum <- as.numeric(table(train$Presence)["1"]) # number of presences
@@ -137,9 +200,9 @@ for (spec in c("SHAL","SHMA","MOFA","MOCA","ANNI","EUAL","FINI")) {
     rf <- randomForest(formula = Presence ~.,
                        data = train,
                        ntree = 1000,
-                       mtry = res$recommended.pars[["mtry"]],
+                       mtry = mtry,
                        sampsize = smpsize,
-                       nodesize = res$recommended.pars[["min.node.size"]],
+                       nodesize = mtry,
                        replace = TRUE)
   } 
   
@@ -151,21 +214,25 @@ for (spec in c("SHAL","SHMA","MOFA","MOCA","ANNI","EUAL","FINI")) {
   # Save model
   saveRDS(rf,file = paste0(proj_path,"SDM/Output/Models/",spec,"_RF_",region,".rds"))
   
-  # predict with RF and RF down-sampled
+  # Predict with RF and RF down-sampled
   rfpred <- predict(rf,test[,fil_vars],type = "prob")[,"1"]
   
-  plot(rf,main = "RF")
-  
-  # calculate area under the ROC and PR curves
+  # Calculate area under the ROC and PR curves
   precrec_obj <- evalmod(scores = rfpred,labels = test[,"Presence"])
   precrec_obj
+  
+  # Get AUC values
+  auc_df <- auc(precrec_obj)
+  
+  model_param["AUC (ROC)",spec] <- auc_df[auc_df$curvetypes == "ROC","aucs"]
+  model_param["AUC (PRC)",spec] <- auc_df[auc_df$curvetypes == "PRC","aucs"]
   
   # Create output folder in directory if does not exist
   if (! dir.exists(paste0(proj_path,"SDM/Output/Evaluation curves"))){
     dir.create(paste0(proj_path,"SDM/Output/Evaluation curves"),recursive = TRUE)
   }
   
-  # plot the ROC and PR curves
+  # Plot the ROC and PR curves
   autoplot(precrec_obj)
   ggsave(paste0(proj_path,"SDM/Output/Evaluation curves/",spec,"_RF_",region,".png"),
          bg = "white")
@@ -176,10 +243,17 @@ for (spec in c("SHAL","SHMA","MOFA","MOCA","ANNI","EUAL","FINI")) {
   }
   
   # Compute variable importance by mean decrease in Gini index
-  imp <- importance(rf,type = 2)
+  imp <- randomForest::importance(rf,type = 2)
   
   # Convert to percentage
   imp <- imp * 100 / sum(imp)
+  
+  # Save variable importance values
+  for (i in used_vars) {
+    if (i %in% row.names(imp)) {
+      model_param[i,spec] <- imp[i,1]
+    }
+  }
   
   # Order variables by importance
   imp <- imp[order(imp[,1]),]
@@ -192,28 +266,41 @@ for (spec in c("SHAL","SHMA","MOFA","MOCA","ANNI","EUAL","FINI")) {
   dev.off()
   
   # Create output folder in directory if does not exist
-  if (! dir.exists(paste0(proj_path,"SDM/Output/Partial dependence plots/",spec,"_RF_",region))){
-    dir.create(paste0(proj_path,"SDM/Output/Partial dependence plots/",spec,"_RF_",region),
+  if (! dir.exists(paste0(proj_path,"SDM/Output/Partial dependence plots"))){
+    dir.create(paste0(proj_path,"SDM/Output/Partial dependence plots"),
                recursive = TRUE)
   }
   
-  # Plot partial dependence of all variables
+  # Plot partial dependence plots in one window
+  png(filename = paste0(proj_path,"SDM/Output/Partial dependence plots/",spec,"_RF_",region,".png"),
+      height = 3000,
+      width = 3000,
+      res = 300)
+  
+  if (spec == "ANNI") {
+    par(mfrow = c(3,3),cex.lab = 1.7)
+  }
+  
+  if (spec != "ANNI") {
+    par(mfrow = c(4,3),cex.lab = 1.7)
+  }
+  
   for (i in seq_along(fil_vars)){
     
-    png(filename = paste0(proj_path,"SDM/Output/Partial dependence plots/",spec,"_RF_",region,"/",fil_vars[i],".png"))
     partialPlot(x = rf,
                 pred.data = train,
                 x.var = fil_vars[i],
                 xlab = fil_vars[i],
-                main = paste("Partial Dependence on", fil_vars[i]),
+                main = NULL,
                 which.class = "1")
-    dev.off()
   }
   
-  # Convert present day predictor variables dataset to dataframe
+  dev.off()
+  
+  # Convert present predictor variables dataset to dataframe
   env_present <- as.data.frame(predictors,xy = TRUE,na.rm = FALSE)
   
-  # Predict RF on entire dataset for present scenario for one class
+  # Predict RF on entire dataset for present scenario
   pred_present <- predict(rf,env_present[,fil_vars],type = "prob")[,"1"]
   
   # Create prediction raster
@@ -231,7 +318,7 @@ for (spec in c("SHAL","SHMA","MOFA","MOCA","ANNI","EUAL","FINI")) {
   
   # Plot prediction raster
   png(filename = paste0(proj_path,"SDM/Output/Prediction/",spec,"_RF_",region,".png"))
-  plot(present_rast)
+  plot(present_rast,range = c(0,1))
   dev.off()
   
   # Save prediction raster
@@ -248,26 +335,32 @@ for (spec in c("SHAL","SHMA","MOFA","MOCA","ANNI","EUAL","FINI")) {
   # Convert dataset to dataframe
   env_past <- as.data.frame(predictors_past,xy = TRUE,na.rm = FALSE)
   
-  # Predict RF on entire dataset for past scenario for one class
+  # Predict RF on entire dataset for past scenario
   pred_past <- predict(rf,env_past[,fil_vars],type = "prob")[,"1"]
   
-  # Create prediction raster
+  # Create past prediction raster
   past_df <- cbind(env_past[,c("x","y")],pred_past)
   
-  past_rast <- rast(past_df,type = "xyz",crs = crs(predictors_past),extent = ext(predictors_past))
+  past_rast <- rast(past_df,
+                    type = "xyz",
+                    crs = crs(predictors_past),
+                    extent = ext(predictors_past))
   
   # Create output folder in directory if does not exist
   if (! dir.exists(paste0(proj_path,"SDM/Output/Past prediction"))){
     dir.create(paste0(proj_path,"SDM/Output/Past prediction"),recursive = TRUE)
   }
   
-  # Plot prediction raster
+  # Plot past prediction raster
   png(filename = paste0(proj_path,"SDM/Output/Past prediction/",spec,"_RF_",region,".png"))
-  plot(past_rast)
+  plot(past_rast,range = c(0,1))
   dev.off()
   
-  # Plot prediction raster
+  # Save past prediction raster
   writeRaster(past_rast,
               file = paste0(proj_path,"SDM/Output/Past prediction/",spec,"_RF_",region,".tif"),
               overwrite = TRUE)
 }
+
+# Write model parameter matrix to CSV file
+write.csv(model_param,file = paste0(proj_path,"SDM/Output/Models/Summary table.csv"))
