4e6083cd00a57ab1eb9f359250511d46e5afa53e
Arunima Jain <arunima.jain00@gmail.com>
2025-04-05 13:00:10 +0530

    .

--- 5. distance.R
+++ 5. distance.R
@@ -13,8 +13,9 @@ proj_path <- "C:/Users/aruni/arunima/IISERTpt/Connectivity/"
 # Raster => Analysis => Proximity (Raster Distance)
 # 1. Select land cover raster as Input
 # 2. Enter values of land cover categories to measure distance to 
-#    in the "target pixel values" blank, separated by commas
-# 3. Select georeferenced coordinates as distance units
+#    in the "target pixel values" field, separated by commas
+# 3. Select georeferenced coordinates as distance units, 
+#    make sure they are in metres
 # 4. Save output file to directory
 
 # Read rasters
@@ -61,7 +62,7 @@ writeRaster(dist_shola_1995,
             filename = paste0(proj_path,"GIS/Derived rasters/Distance/Extra/dist_shola_1995_30m.tif"),
             overwrite = TRUE)
 
-## Do the same for distance to woodland edge
+## Do the same for distance to woodland edge and distance to grassland edge
 
 dist_woodland_2017_temp <- rast(paste0(proj_path,"GIS/Derived rasters/Distance/Extra/dist_woodland_2017_30m_temp.tif"))
 dist_woodland_1995_temp <- rast(paste0(proj_path,"GIS/Derived rasters/Distance/Extra/dist_woodland_1995_30m_temp.tif"))
@@ -102,18 +103,51 @@ writeRaster(dist_woodland_1995,
             filename = paste0(proj_path,"GIS/Derived rasters/Distance/Extra/dist_woodland_1995_30m.tif"),
             overwrite = TRUE)
 
-# Read distance to settlement and grassland rasters
-dist_smts_2017 <- rast(paste0(proj_path,"GIS/Derived rasters/Distance/Extra/dist_settlements_2017_30m.tif"))
-dist_smts_1995 <- rast(paste0(proj_path,"GIS/Derived rasters/Distance/Extra/dist_settlements_1995_30m.tif"))
+dist_grassland_2017_temp <- rast(paste0(proj_path,"GIS/Derived rasters/Distance/Extra/dist_grassland_2017_30m_temp.tif"))
+dist_grassland_1995_temp <- rast(paste0(proj_path,"GIS/Derived rasters/Distance/Extra/dist_grassland_1995_30m_temp.tif"))
 
-dist_grassland_2017 <- rast(paste0(proj_path,"GIS/Derived rasters/Distance/Extra/dist_grassland_2017_30m_temp.tif"))
-dist_grassland_1995 <- rast(paste0(proj_path,"GIS/Derived rasters/Distance/Extra/dist_grassland_1995_30m_temp.tif"))
+dist_grassland_2017_inv <- rast(paste0(proj_path,"GIS/Derived rasters/Distance/Extra/dist_grassland_2017_30m_inv.tif"))
+dist_grassland_1995_inv <- rast(paste0(proj_path,"GIS/Derived rasters/Distance/Extra/dist_grassland_1995_30m_inv.tif"))
 
-names(dist_smts_2017) <- "dist_settlements"
-names(dist_smts_1995) <- "dist_settlements"
+dist_grassland_2017_inv <- (0 - dist_grassland_2017_inv)
+dist_grassland_1995_inv <- (0 - dist_grassland_1995_inv)
+
+dist_grassland_2017_inv <- mask(dist_grassland_2017_inv,
+                               mask = dist_grassland_2017_temp,
+                               maskvalues = 0,
+                               inverse = TRUE,
+                               updatevalue = NA)
+dist_grassland_1995_inv <- mask(dist_grassland_1995_inv,
+                               mask = dist_grassland_1995_temp,
+                               maskvalues = 0,
+                               inverse = TRUE,
+                               updatevalue = NA)
+
+dist_grassland_2017 <- merge(x = dist_grassland_2017_inv,
+                            y = dist_grassland_2017_temp,
+                            first = TRUE,
+                            na.rm = TRUE)
+dist_grassland_1995 <- merge(x = dist_grassland_1995_inv,
+                            y = dist_grassland_1995_temp,
+                            first = TRUE,
+                            na.rm = TRUE)
 
 names(dist_grassland_2017) <- "dist_grassland"
 names(dist_grassland_1995) <- "dist_grassland"
+
+writeRaster(dist_grassland_2017,
+            filename = paste0(proj_path,"GIS/Derived rasters/Distance/Extra/dist_grassland_2017_30m.tif"),
+            overwrite = TRUE)
+writeRaster(dist_grassland_1995,
+            filename = paste0(proj_path,"GIS/Derived rasters/Distance/Extra/dist_grassland_1995_30m.tif"),
+            overwrite = TRUE)
+
+# Read distance to settlement rasters
+dist_smts_2017 <- rast(paste0(proj_path,"GIS/Derived rasters/Distance/Extra/dist_settlements_2017_30m.tif"))
+dist_smts_1995 <- rast(paste0(proj_path,"GIS/Derived rasters/Distance/Extra/dist_settlements_1995_30m.tif"))
+
+names(dist_smts_2017) <- "dist_settlements"
+names(dist_smts_1995) <- "dist_settlements"
 
 # Read reference 1ha resolution raster
 rast_1ha <- rast(paste0(proj_path,"occupancy data/1500 1ha grids/1ha grids.tif"))
