b85b624a4a58120c44b2de4cb1507224e2ce14ff
Arunima Jain <arunima.jain00@gmail.com>
2025-06-22 21:12:45 +0530

    Remove 25ha

--- 3. topo.R
+++ 3. topo.R
@@ -27,48 +27,29 @@ names(twi) <- "TWI"
 # Read reference 1ha resolution raster
 rast_1ha <- rast(paste0(proj_path,"occupancy data/1500 1ha grids/1ha grids.tif"))
 
-# Read reference 25ha raster
-clim_zone <- rast(paste0(proj_path,"GIS/Climate zones/clim_zone_25ha.tif"))
-
 # Create loop to derive all topographic variables
 for (i in c("aspect","slope","TPI","TRI","roughness")) {
   
   # Derive topographic variable from DEM
   topo <- terrain(dem,v = i)
   
-  # Reproject to 1ha and 25ha rasters and write to TIF files
+  # Reproject to 1ha raster and write to TIF files
   topo_1ha <- project(x = topo,
                       y = rast_1ha,
                       method = "med",
                       filename = paste0(proj_path,"GIS/Derived rasters/Topo variables/1ha/",i,"_1ha.tif"),
                       overwrite = TRUE)
-  
-  topo_25ha <- project(x = topo,
-                       y = clim_zone,
-                       method = "med",
-                       filename = paste0(proj_path,"GIS/Derived rasters/Topo variables/25ha/",i,"_25ha.tif"),
-                       overwrite = TRUE)
 }
 
-# Reproject DEM and TWI to 1ha and 25ha resolutions and write to TIF files
+# Reproject DEM and TWI to 1ha resolution and write to TIF files
 dem_1ha <- project(x = dem,
                    y = rast_1ha,
                    method = "med",
                    filename = paste0(proj_path,"GIS/Derived rasters/Topo variables/1ha/elevation_1ha.tif"),
                    overwrite = TRUE)
-dem_25ha <- project(x = dem,
-                    y = clim_zone,
-                    method = "med",
-                    filename = paste0(proj_path,"GIS/Derived rasters/Topo variables/25ha/elevation_25ha.tif"),
-                    overwrite = TRUE)
 
 twi_1ha <- project(x = twi,
                    y = rast_1ha,
                    method = "med",
                    filename = paste0(proj_path,"GIS/Derived rasters/Topo variables/1ha/TWI_1ha.tif"),
                    overwrite = TRUE)
-twi_25ha <- project(x = twi,
-                    y = clim_zone,
-                    method = "med",
-                    filename = paste0(proj_path,"GIS/Derived rasters/Topo variables/25ha/TWI_25ha.tif"),
-                    overwrite = TRUE)
